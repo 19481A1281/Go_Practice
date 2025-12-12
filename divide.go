8693f935f93c33d19f3b823f74ce41edc0f1b94c
Durga Prasad <durga.pn@techfino.in>
2025-12-12 18:51:54 +0530

    error handling

---
+++ divide.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+func divide(x, y float64) (float64, error) {
+	if y == 0 {
+		return 0, errors.New("divide by zero error")
+	}
+
+	return x / y, nil
+}
+
+func main() {
+	result, err := divide(10, 0)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	fmt.Println("Result:", result)
+}
